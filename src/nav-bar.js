d9825199f78c2fb20393e72f9f2cc5194a6bfaa2
ahmet demir <ahmet.demir@medyasoft.com.tr>
2022-05-27 02:26:19 +0300

    updated

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -1,21 +1,23 @@
- const navbtn = document.getElementById("nav-btn");
- const content =  document.getElementById("content");
- const header = document.getElementById("header");
- const headerPart = document.getElementById("header-part");
- navbtn.onclick = function(e){
-     console.log("cliced");
+const navbtn = document.getElementById("nav-btn");
+const content = document.getElementById("content");
+const header = document.getElementById("header");
+const headerPart = document.getElementById("header-part");
+navbtn.onclick = function (e) {
+    console.log("cliced");
     content.style.marginLeft = "40%";
-     header.style.width = "40%";
-     headerPart.style.display = "block"
-    
-     e.preventDefault();
- }
+    header.style.width = "40%";
+    headerPart.style.display = "block"
+    navbtn.classList.add("d-none");
+    navbtn.classList.remove("d-lg-block");
+    e.preventDefault();
+}
 
- document.getElementById("content").onclick = function(){
-     console.log("body");
-     document.getElementById("content").style.marginLeft = 0;
-     header.style.width = "0%";
- headerPart.style.display = "none"
+document.getElementById("content").onclick = function () {
+    console.log("body");
+    document.getElementById("content").style.marginLeft = 0;
+    header.style.width = "0%";
+    headerPart.style.display = "none";
+    navbtn.classList.remove("d-none");
+    navbtn.classList.add("d-lg-block");
+}
 
- }
- 
