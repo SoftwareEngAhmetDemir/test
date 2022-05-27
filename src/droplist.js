36d050c7687af2a6d36fc7592feb24040028a5be
ahmet demir <ahmet.demir@medyasoft.com.tr>
2022-05-28 01:48:02 +0300

    hghv

---
+++ src/droplist.js
@@ -0,0 +1,17 @@
+
+    // $('.dropdownMenuButton2').on("click",function() {
+    //     console.log("geldi");
+    
+    //     jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    //   }, function() {
+    //   jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    // });
+
+$(".dropdown").on("click",function(){
+    console.log("geldi");
+   
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).fadeIn(100);
+    }, function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(200).fadeOut(300);
+    
+})
