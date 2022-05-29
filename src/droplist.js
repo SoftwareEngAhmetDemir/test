e749bac39d836c60d9c7593e86128ab100276d93
ahmet demir <ahmet.demir@medyasoft.com.tr>
2022-05-29 07:01:23 +0300

    modal

--- src/droplist.js
+++ src/droplist.js
@@ -7,11 +7,11 @@
     //   jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
     // });
 
-$(".dropdown").on("click",function(){
-    console.log("geldi");
+// $(".dropdown").on("click",function(){
+//     console.log("geldi");
    
-        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).fadeIn(100);
-    }, function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).delay(200).fadeOut(300);
+//         $(this).find('.dropdown-menu').first().stop(true, true).delay(100).fadeIn(100);
+//     }, function() {
+//         $(this).find('.dropdown-menu').first().stop(true, true).delay(200).fadeOut(300);
     
-})
+// })
